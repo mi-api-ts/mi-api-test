91ce6d806c6a0a170219985f01235b395c4bf56d
Omar <albertocoronado2025@gmail.com>
2026-03-27 11:04:45 -0400

    Initial commit: arreglo de cors segunda vuelta2

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 // ============================================================================
 
 app.use(cors({
-  origin: ['http://localhost:4200', 'http://127.0.0.1:4200',, 'https://mi-api-test-1.onrender.com'],
+  origin: ['http://localhost:4200', 'http://127.0.0.1:4200','https://mi-api-test-1.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
